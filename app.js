3023636df44defa9669474af735bd284f6e4b968
Serhii Kyslovets <serhii.love@gmail.com>
2022-04-20 14:12:13 +0300

    chore: move routes in separate module

--- app.js
+++ app.js
@@ -1,35 +1,7 @@
 const http = require('http')
-const fs = require('fs')
 
-const server = http.createServer((req, res) => {
-    const url = req.url
-    const method = req.method
+const routes = require('./routes')
 
-    if (url === '/') {
-        res.write('<html>');
-        res.write('<head><title>My first page!</title></head>');
-        res.write('<body><form action="/message" method="POST"><input type="text" name="message"/><button>Send</button></form></body>');
-        res.write('</html>');
-
-        return res.end();
-    }
-    if (url === '/message' && method === 'POST') {
-        const body = []
-        req.on('data', (chunk) => {
-            body.push(chunk)
-        });
-        req.on('end',() => {
-            const parsedBody = Buffer.concat(body).toString()
-            const message = parsedBody.split('=')[1];
-            fs.writeFileSync('message', message);
-        })
-        res.statusCode = 302;
-        res.setHeader('Location', '/');
-
-        return res.end();
-    }
-
-    res.setHeader('Content-type', 'text/html')
-})
+const server = http.createServer(routes)
 
 server.listen(3000)
---
+++ routes.js
@@ -0,0 +1,38 @@
+const fs = require('fs')
+
+const handleRequest = (req, res) => {
+    const url = req.url
+    const method = req.method
+
+    if (url === '/') {
+        res.write('<html>');
+        res.write('<head><title>My first page!</title></head>');
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"/><button>Send</button></form></body>');
+        res.write('</html>');
+
+        return res.end();
+    }
+    if (url === '/message' && method === 'POST') {
+        const body = []
+        req.on('data', (chunk) => {
+            body.push(chunk)
+        });
+        req.on('end',() => {
+            const parsedBody = Buffer.concat(body).toString()
+            const message = parsedBody.split('=')[1];
+            fs.writeFileSync('message', message);
+        })
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+
+        return res.end();
+    }
+
+    res.setHeader('Content-type', 'text/html')
+    res.write('<head><title>My first page!</title></head>');
+    res.write('<body><p>This is default page</p></body>');
+    res.write('</html>');
+}
+
+module.exports = handleRequest
+
