8e2f595a6f1b357bcbc6327bf500000b01b91677
Serhii Kyslovets <serhii.love@gmail.com>
2022-11-01 14:12:39 +0200

    chore: minor update

--- assignment-2/app.js
+++ assignment-2/app.js
@@ -2,9 +2,15 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.use((req, res, next) => {
+    console.log('Request type: ', req.method);
+    next();
+})
+
 app.get('/', (req, res) => {
-    res.send('Hello world!')
+    res.send('Hello world!');
 });
+
 
 app.listen(port, () => {
     console.log(`App listening on port ${port}`);
