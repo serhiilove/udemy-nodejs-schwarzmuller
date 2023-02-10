ccd4073d6f3f7d8cf61ea74274a6e1fc80ef3374
Serhii Kyslovets <serhii.love@gmail.com>
2023-02-10 10:26:04 +0200

    feat: updated add and get products functionality

--- section-13/models/product.js
+++ section-13/models/product.js
@@ -1,3 +1,28 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const productsSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    imageUrl: {
+        type: String,
+        required: true
+    }
+});
+
+module.exports = mongoose.model('Product', productsSchema);
+
+
 // const mongodb = require('mongodb');
 // const getDb = require('../util/database').getDb;
 
