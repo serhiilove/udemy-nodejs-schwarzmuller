6d91a5e5a4e532a0403b3f2690fe18b446501622
Serhii Kyslovets <serhii.love@gmail.com>
2023-03-01 10:21:35 +0200

    chore: added isLoggedIn flag to the Cookies

--- section-14/controllers/auth.js
+++ section-14/controllers/auth.js
@@ -1,6 +1,15 @@
 exports.getLogin = (req, res, next) => {
+    const allCookies = req.get('Cookie').split(';');
+    const isLoggedIn = allCookies[allCookies.length - 1].split('=')[1].trim() === 'true';
+
     res.render('auth/login', {
         path: '/login',
-        pageTitle: 'Login'
+        pageTitle: 'Login',
+        isAuthenticated: isLoggedIn
     });
 };
+
+exports.postLogin = (req, res, next) => {
+    res.setHeader('Set-Cookie', 'loggedIn=true');
+    res.redirect('/');
+}
