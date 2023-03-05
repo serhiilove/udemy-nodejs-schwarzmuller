f017cd80b88367741104c6e5458155139036f9b8
Serhii Kyslovets <serhii.love@gmail.com>
2023-03-05 10:15:47 +0200

    chore: add logout functionality

--- section-14/controllers/auth.js
+++ section-14/controllers/auth.js
@@ -1,8 +1,6 @@
 const User = require('../models/user');
 
 exports.getLogin = (req, res, next) => {
-    console.log(req.session.isLoggedIn)
-
     res.render('auth/login', {
         path: '/login',
         pageTitle: 'Login',
@@ -13,10 +11,16 @@ exports.getLogin = (req, res, next) => {
 exports.postLogin = (req, res, next) => {
     User.findById('63e74b372a4d35369f2c5f3f')
         .then(user => {
-            console.log('user: ', user);
             req.session.user = user;
             req.session.isLoggedIn = true;
             res.redirect('/');
         })
         .catch(err => console.log(err));
+}
+
+exports.postLogout = (req, res, next) => {
+    req.session.destroy(err => {
+        console.log(err);
+        res.redirect('/');
+    });
 }
