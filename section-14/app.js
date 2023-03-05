ce8bafed7fc3a8cdcac44539aaae818b7c75a1f3
Serhii Kyslovets <serhii.love@gmail.com>
2023-03-05 10:04:03 +0200

    chore: updated authentication process

--- section-14/app.js
+++ section-14/app.js
@@ -3,11 +3,19 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
+
+const MONGODB_URI = 'mongodb+srv://serhiilove:vhYAbVR2wTC41Sql@cluster0.bqruq6t.mongodb.net/shop?retryWrites=true&w=majority';
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
 
 const app = express();
+const store = new MongoDBStore({
+  uri: MONGODB_URI,
+  collection: 'sessions'
+});
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -16,17 +24,24 @@ const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const authRoutes = require('./routes/auth');
 
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+  secret: 'my secret',
+  resave: false,
+  saveUninitialized: false,
+  store: store
+}));
 
-app.use((req, res, next) => {
-  User.findById('5bab316ce0a7c75f783cb8a8')
-    .then(user => {
-      req.user = user;
-      next();
-    })
-    .catch(err => console.log(err));
-});
+// app.use((req, res, next) => {
+//   User.findById('5bab316ce0a7c75f783cb8a8')
+//     .then(user => {
+//       req.user = user;
+//       next();
+//     })
+//     .catch(err => console.log(err));
+// });
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
@@ -36,7 +51,7 @@ app.use(errorController.get404);
 
 mongoose
   .connect(
-    'mongodb+srv://serhiilove:vhYAbVR2wTC41Sql@cluster0.bqruq6t.mongodb.net/shop?retryWrites=true&w=majority'
+    MONGODB_URI
   )
   .then(result => {
     User.findOne().then(user => {
--- section-14/controllers/auth.js
+++ section-14/controllers/auth.js
@@ -1,15 +1,22 @@
+const User = require('../models/user');
+
 exports.getLogin = (req, res, next) => {
-    const allCookies = req.get('Cookie').split(';');
-    const isLoggedIn = allCookies[allCookies.length - 1].split('=')[1].trim() === 'true';
+    console.log(req.session.isLoggedIn)
 
     res.render('auth/login', {
         path: '/login',
         pageTitle: 'Login',
-        isAuthenticated: isLoggedIn
+        isAuthenticated: req.session.isLoggedIn
     });
 };
 
 exports.postLogin = (req, res, next) => {
-    res.setHeader('Set-Cookie', 'loggedIn=true');
-    res.redirect('/');
+    User.findById('63e74b372a4d35369f2c5f3f')
+        .then(user => {
+            console.log('user: ', user);
+            req.session.user = user;
+            req.session.isLoggedIn = true;
+            res.redirect('/');
+        })
+        .catch(err => console.log(err));
 }
